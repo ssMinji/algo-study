7dbafdb1f8e05bcbfe30dc6c45e53723d951f9a3
Minji Song <44194800+ssMinji@users.noreply.github.com>
2024-09-19 17:47:07 +0900

    Update TwoSum.java

--- Java/TwoSum.java
+++ Java/TwoSum.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-
+//twoSum (https://leetcode.com/problems/two-sum/)
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length-1; i++){
